f6c3605145ceaef26d368dc1d7a446233626359b
Joshua Ebenezer <joshuaebenezer@utexas.edu>
2021-10-06 13:26:53 -0500

    chipqa_pq

--- svr.py
+++ svr.py
@@ -51,9 +51,9 @@ def results(all_preds,all_dmos):
 
 
 
-scores_df = pd.read_csv('../hdr_4k_mos_and_dmos.csv')
-video_names = scores_df['video']
-scores = scores_df['dmos']
+scores_df = pd.read_csv('./fall2021_yuv_rw_info.csv')
+video_names = scores_df['yuv']
+scores = scores_df['mos']
 print(len(scores_df['content'].unique()))
 srocc_list = []
 
@@ -66,7 +66,7 @@ def trainval_split(trainval_content,r):
     val_scores = []
 #    feature_folder= "/home/ubuntu/bitstream_mode3_p1204_3/features/p1204_etri_features"
 
-    feature_folder= './hdr_chipqa_pq_features'
+    feature_folder= './features/fall21_hdr_chipqa_pq_features'
     train_names = []
     val_names = [] 
     for i,vid in enumerate(video_names):
@@ -85,12 +85,12 @@ def trainval_split(trainval_content,r):
             train_features.append(feature)
             train_scores.append(score)
             train_indices.append(i)
-            train_names.append(scores_df.loc[i]['video'])
+            train_names.append(scores_df.loc[i]['yuv'])
             
         elif(scores_df.loc[i]['content'] in val):
             val_features.append(feature)
             val_scores.append(score)
-            val_names.append(scores_df.loc[i]['video'])
+            val_names.append(scores_df.loc[i]['yuv'])
 #    print('Train set')
 #    print(len(train_names))
 #    print('Validation set')
